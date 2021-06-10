19858ff3ac9662eef47eabcdbe54d4277299528a
AuguWii <tejones.krisha@gmail.com>
2021-06-10 14:58:50 +0800

    minor changes

---
+++ lib/capture.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CamClassify extends StatefulWidget {
+  const CamClassify({Key? key}) : super(key: key);
+
+  @override
+  _CamClassifyState createState() => _CamClassifyState();
+}
+
+class _CamClassifyState extends State<CamClassify> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
